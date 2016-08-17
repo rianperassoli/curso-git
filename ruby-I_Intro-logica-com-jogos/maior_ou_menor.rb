be76adfc771bf0a23427ca577d1965ded6219b1b
Rian Perassoli <rian.perassoli@gmail.com>
2016-08-16 22:45:00 -0300

    Ruby Aula 2

--- ruby-I_Intro-logica-com-jogos/maior_ou_menor.rb
+++ ruby-I_Intro-logica-com-jogos/maior_ou_menor.rb
@@ -9,11 +9,31 @@ puts "Escolhendo um número randômico entre 0 e 200..."
 numero_randomico = 175
 
 puts "Escolhido. Que tal adivinhar hoje o nosso número secreto?"
-puts "\n\n\n\n"
-puts "Tentativa 1"
-puts "Digite um número"
-chute = gets
-puts "\n\n"
-puts "Será que acertou? Você chutou " + chute
 
-puts chute.to_i == numero_randomico
+puts "\n\n"
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+	puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+	puts "Digite um número"
+	chute = gets
+	puts "Será que acertou? Você chutou " + chute
+
+	acertou = numero_randomico == chute.to_i
+
+	if acertou
+		puts "Acertou!"
+		break
+	else
+		maior = numero_randomico > chute.to_i
+		if maior
+			puts "Errou! O numero secreto é maior."
+		else
+			puts "Errou! O numero secreto é menor."
+		end
+	end
+end
+
+
+
--- ruby-I_Intro-logica-com-jogos/teste.rb
+++ ruby-I_Intro-logica-com-jogos/teste.rb
@@ -13,3 +13,18 @@ puts 175 >= 174
 puts 175 <= 175
 puts 175 <= 174
 175.to_i
+
+valor_inicial = 1
+valor_final = 3
+
+puts "Vou contar de " + valor_inicial.to_s + " até " + valor_final.to_s
+for contar in valor_inicial..valor_final
+	puts "Contando: " + contar.to_s
+end
+
+=begin
+
+for s in "A".."z"
+	puts "Contando " + s
+end
+=end
