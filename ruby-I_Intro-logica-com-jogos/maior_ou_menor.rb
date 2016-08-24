07e27bcd724ae1beaf8cdde9c4e4f35954ebe44e
Rian Perassoli <rian.perassoli@gmail.com>
2016-08-23 21:39:20 -0300

    Ruby Aulas 4 e 5

--- ruby-I_Intro-logica-com-jogos/maior_ou_menor.rb
+++ ruby-I_Intro-logica-com-jogos/maior_ou_menor.rb
@@ -1,24 +1,25 @@
 def dar_boas_vindas
 	puts "Bem vindo ao jogo da adivinhação"
 	puts "Qual é o seu nome?"
-	nome = gets
+	nome = gets.strip
 	puts "\n"
-	puts "Começaremos o jogo para você, " + nome
+	puts "Começaremos o jogo para você, #{nome}"
 end
 
 def sorteia_numero_randomico
 	puts "Escolhendo um número randômico entre 0 e 200..."
-	sorteado = 175
+	sorteado = rand(200)
 	puts "Escolhido. Que tal adivinhar hoje o nosso número secreto?"	
 	sorteado
 end
 
-def pede_um_numero(tentativa, limite_de_tentativas)
+def pede_um_numero(chutes, tentativa, limite_de_tentativas)
 	puts "\n\n"
-	puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+	puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
+	puts "Chutes até agora: #{chutes}"
 	puts "Digite um número"
-	chute = gets
-	puts "Será que acertou? Você chutou " + chute
+	chute = gets.strip
+	puts "Será que acertou? Você chutou #{chute}"
 	return chute.to_i
 end
 
@@ -39,17 +40,27 @@ def verifica_se_acertou(numero_randomico, chute)
 	false
 end
 
-dar_boas_vindas
+dar_boas_vindas	
 
 numero_randomico = sorteia_numero_randomico
 
 limite_de_tentativas = 5
+pontos_ate_agora = 1000
+
+chutes = []
 
 for tentativa in 1..limite_de_tentativas
-	
-	chute = pede_um_numero tentativa, limite_de_tentativas
+
+	chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+
+	pontos_a_perder = (chute - numero_randomico).abs / 2.0
+	pontos_ate_agora -= pontos_a_perder
+
+	chutes << chute	
 
 	if verifica_se_acertou numero_randomico, chute
 		break
 	end
 end
+
+puts "Você ganhou #{pontos_ate_agora} pontos."
--- ruby-I_Intro-logica-com-jogos/teste.rb
+++ ruby-I_Intro-logica-com-jogos/teste.rb
@@ -1,3 +1,64 @@
+treze = 13
+cinco = 5
+
+puts treze / cinco #divisão
+puts treze % cinco #resto da divisao
+puts treze + cinco #adição
+puts treze - cinco #subtração
+puts treze * cinco #multiplicação
+
+
+numero = 5
+numero += 1
+numero -= 2
+numero *= 2
+numero /= 4
+numero *= 13
+numero %= 10
+puts numero 
+
+
+
+chutes = [50, 150, 300]
+for i in 1..chutes.size
+  puts "Chute #{i}: #{chutes[i - 1]}"
+end
+
+
+nome=gets.strip
+puts nome.size.to_s + " caracteres"
+puts "#{nome.size} caracteres"
+
+
+nome=gets.strip
+puts nome.size.to_s + " caracteres"
+
+
+chutes = [176, 100, 130, 150, 175]
+
+puts chutes[0]
+puts chutes[1]
+puts chutes[2]
+puts chutes[3]
+puts chutes[4]
+
+chutes = []
+
+chute = 150
+chutes << chute
+
+chute = 180
+chutes << chute
+
+chute = 200
+chutes << chute
+
+puts chutes.size
+for chute in chutes
+	puts chute
+end
+
+
 puts 175 == 175
 puts 175 == 174
 
@@ -28,3 +89,5 @@ for s in "A".."z"
 	puts "Contando " + s
 end
 =end
+
+
