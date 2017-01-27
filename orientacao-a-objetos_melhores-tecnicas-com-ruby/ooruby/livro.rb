f269f7b18a3d9fdbd9fb0299132f1e88b5118b83
Rian Perassoli <rian.perassoli@gmail.com>
2017-01-26 22:17:48 -0200

    Aula 8, Curso de orientacao a objetos com ruby

--- orientacao-a-objetos_melhores-tecnicas-com-ruby/ooruby/livro.rb
+++ orientacao-a-objetos_melhores-tecnicas-com-ruby/ooruby/livro.rb
@@ -1,25 +1,17 @@
 require_relative "produto"
+require_relative "impresso"
 
-class Livro
-	include Produto
+class Livro < Produto	
+	include Impresso
 
 	def initialize(titulo, preco, ano_lancamento, possui_reimpressao, editora, possui_sobrecapa)
-		@titulo = titulo
-		@ano_lancamento = ano_lancamento
+		super(titulo, preco, ano_lancamento, editora)
 		@possui_reimpressao = possui_reimpressao
-		@preco  = calcula_preco(preco)
-		@editora = editora
 		@possui_sobrecapa = possui_sobrecapa
-	end
-
-	def possui_reimpressao?
-		@possui_reimpressao
 	end
 
 	def matches?(query)
 	  ["livro", "impresso"].include?(query)
 	end
-
-
 
 end
--- orientacao-a-objetos_melhores-tecnicas-com-ruby/ooruby/revista.rb
+++ orientacao-a-objetos_melhores-tecnicas-com-ruby/ooruby/revista.rb
@@ -1,19 +1,13 @@
 require_relative "produto"
+require_relative "impresso"
 
-class Revista
-	include Produto
+class Revista < Produto
+	include Impresso
 
 	def initialize(titulo, preco, ano_lancamento, possui_reimpressao, editora, numero)
-		@titulo = titulo
-		@ano_lancamento = ano_lancamento
+		super(titulo, preco, ano_lancamento, editora)
 		@possui_reimpressao = possui_reimpressao
-		@preco  = calcula_preco(preco)
-		@editora = editora
 		@numero = numero
-	end
-
-	def possui_reimpressao?
-		@possui_reimpressao
 	end
 
   def matches?(query)
