1d604ee2c3bf0280a4cf340cfb400f060beb7673
Rian Perassoli <rian.perassoli@gmail.com>
2017-05-02 22:37:07 -0300

    Adicionando botão delete e link para a página de adicionar Produto. Criando rotas de forma mais inteligentes utilizando os recursos do rails

--- ruby-on-rails-4_Do-zero-a-web/lojadecamisetas/config/routes.rb
+++ ruby-on-rails-4_Do-zero-a-web/lojadecamisetas/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  post "/produtos" => "produtos#create"
-  get "/produtos/new" => "produtos#new"
-  root "produtos#index"   
+	resources :produtos, only: [:new, :create, :destroy]
+	
+	# A linha acima substitui esses metodos/rotas abaixo
+	#post "/produtos" => "produtos#create"
+    #get "/produtos/new" => "produtos#new"
+    #delete "/produtos/:id" => "produtos#destroy", as: :produto
+  
+    root "produtos#index"   
 end
