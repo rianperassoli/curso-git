c6032ca58fafa95be61cccd1d5fea6ef92594bd5
Rian Perassoli <rian.perassoli@gmail.com>
2017-05-18 20:50:07 -0300

    Implementação da consulta de produtos na barra de navegacao

--- ruby-on-rails-4_Do-zero-a-web/lojadecamisetas/config/routes.rb
+++ ruby-on-rails-4_Do-zero-a-web/lojadecamisetas/config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 	#post "/produtos" => "produtos#create"
     #get "/produtos/new" => "produtos#new"
     #delete "/produtos/:id" => "produtos#destroy", as: :produto
-  
+  	get "/produtos/busca" => "produtos#busca", as: :busca_produto
     root "produtos#index"   
 end
